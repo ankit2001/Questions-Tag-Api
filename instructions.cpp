c035570b85eb7f584b20a220921e6bad4804dfd7
ankit2001 <ankitchoudhary202.ac@gmail.com>
2020-06-26 22:56:22 +0530

    Add prototype 1

---
+++ instructions.cpp
@@ -0,0 +1,18 @@
+I have used stackoverflow dataset to train the model, it classifies random text to programming languages like cpp, java, etc. The final approach will be as same as this if i get the abuse and drugs dataset.
+
+Instructions to check the model
+
+1. Open the google colab notebook.
+2. Use shift + enter to run any cell.
+3. Run all the cells from top to bottom.
+4. In last sixth cell you can see the results predicted by model, TEXT field belongs to any random text or string, PREDICTED TAGS BY MODEL field belongs to tag predicted by my model and TAG IT REALLY HAD ON STACKOVERFLOW field belongs to acutal tag on stackoverflow.
+5. You can check random text in last 4th cell, i have written comments to check any random text like 
+"""
+Test your string here
+DEMO:
+TEXT.append("How to delete numpy arrays")
+"""
+Just add any text below it in TEXT.append("")
+Example: TEXT.append("How to use cin and cout")
+Use shift + enter and it will show the output below.
+(c++) for this example.
